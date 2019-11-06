5d7b0808084a26e4542c82fc9660c4d4a7185ca3
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-11-06 21:52:57 +0100

    Better radio fetching

---
+++ actual-site/index.js
@@ -0,0 +1,26 @@
+const radioList = document.querySelector("select");
+const audio = document.querySelector("audio");
+
+async function getRadios() {
+  const result = await fetch("radios.json");
+  return await result.json();
+}
+
+getRadios().then(radios => {
+  let option;
+  for (let radio of radios) {
+    option = document.createElement("option");
+    option.value = radio.url;
+    option.textContent = radio.title;
+    radioList.appendChild(option);
+  }
+  radioList.onchange = ev => {
+    audio.src = ev.target.value;
+    audio.play();
+    localStorage.setItem("current_radio", audio.src);
+    localStorage.setItem("option_index", radioList.selectedIndex);
+  };
+
+  audio.src = localStorage.getItem("current_radio");
+  radioList.selectedIndex = localStorage.getItem("option_index");
+});
