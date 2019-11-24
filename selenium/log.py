ee882c426a403403e1438f3a05b8a9e0a62cc2be
hwadii <wadii.hajji@etu.univ-amu.fr>
2019-11-24 20:18:40 +0100

    Scrape with selenium

--- selenium/log.py
+++ selenium/log.py
@@ -1,15 +1,27 @@
+import json
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 from scrapy_selenium import SeleniumRequest
-
-user_name = "wadii"
-password = "secret"
 
 driver = webdriver.Firefox()
 driver.get("https://doc.ubuntu-fr.org/liste_radio_france")
-elements = driver.find_element_by_css_selector("li.node")
+elements = driver.find_elements_by_css_selector("li.node")
+result = []
+
 for element in elements:
-    print(element)
+    title = element.find_element_by_css_selector("div.li > a.urlextern")
+    title = title.text
+    try:
+        url = element.find_element_by_css_selector(
+            "ul.fix-media-list-overlap > li.level2 > div.li > a.urlextern")
+        url = url.get_attribute("href")
+    except:
+        url = ""
+    item = {"title": title, "url": url}
+    result.append(item)
+
 driver.close()
 
-
+with open("radios.json", "w") as f:
+    json.dump(result, f, indent=2, sort_keys=True)
